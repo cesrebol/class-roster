8da02052b79ec20be4c0e13b6a5635f759f475ca
cesar.rebol <cesar.rebol@gmail.com>
2023-07-27 20:39:28 -0400

    adding files to repo

---
+++ degree.h
@@ -0,0 +1,10 @@
+include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World" << endl;
+    bool isMale = true;
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "roster.h"
+using namespace std;
+
+int main()
+{
+    cout << "hello world!";
+    return 0;
+
+
+}
