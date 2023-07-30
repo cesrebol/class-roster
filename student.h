6e39f4f6b1c23c4a508988f4892afedddb64f380
cesar.rebol <cesar.rebol@gmail.com>
2023-07-30 15:28:42 -0400

    Create the class Student in student.h

---
+++ student.h
@@ -0,0 +1,15 @@
+#include <iostream> 
+#include "degree.h" // including degree.h file for this class
+
+using namespace std;
+
+class Student {
+    public:
+        string studentId;
+        string firstName;
+        string lastName;
+        string emailAddress;
+        int age;
+        int daysInCourse;
+        string degreeProgram;
+};
