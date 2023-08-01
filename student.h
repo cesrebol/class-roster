3d8788f09ee18345c21c40de3da10b4986cbbd95
cesar.rebol <cesar.rebol@gmail.com>
2023-08-01 14:18:49 -0400

    getters and setters completed in student.h

--- student.h
+++ student.h
@@ -10,9 +10,29 @@ class Student {
         string lastName;
         string emailAddress;
         int age;
-        int daysInCourse;
+        int daysInCourse[3];
         DegreeProgram degreeProgram;
     public:
         Student(string studentId, string firstName, string lastName, string emailAddress, int age, int daysInCourse, DegreeProgram degreeProgram);
+        string getStudentId(); 
+        string getFirstName();
+        string getLastName();
+        string getEmailAddress();
+        int getAge();
+        int* getDaysInCourse();
+        DegreeProgram getDegreeProgram();        
+
+        void setStudenId(string studentId);
+        void setFirstName(string firstName);
+        void setLastName(string lastName);
+        void setEmailAddress(string emailAddress);
+        void setAge(int age);
+        void setDaysInCourse(int* daysInCourse);
+        void setDegreeProgram(DegreeProgram degreeProgram);
         
+        void print();
+
+
+
+
 };
